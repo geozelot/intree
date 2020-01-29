4af31c2832f1989ad8f9f393de6004a745aee037
geozelot <info@geozelot.com>
2020-01-29 11:47:21 +0100

    gofmt'ed files

--- intree.go
+++ intree.go
@@ -1,13 +1,9 @@
 package intree
 
 import (
-
-  "math"
-  "math/rand"
-
+	"math"
+	"math/rand"
 )
-
-
 
 /*
  *  Main interface expected by NewINTree();
@@ -16,11 +12,8 @@ import (
  */
 
 type Bounds interface {
-
-  Limits() (L, U float64)
-
+	Limits() (L, U float64)
 }
-
 
 /*
  *  Simple struct implicitly implementing Bounds interface;
@@ -32,24 +25,20 @@ type Bounds interface {
  */
 
 type SimpleBounds struct {
-
-  Lower, Upper float64
-
+	Lower, Upper float64
 }
 
+/*
+ *  Implementing Bounds interface method
+ *
+ *    @return:  returns lower and upper limit of bounds
+ */
 
-  /*
-   *  Implementing Bounds interface method
-   *
-   *    @return:  returns lower and upper limit of bounds
-   */
+func (sb *SimpleBounds) Limits() (float64, float64) {
 
-  func (sb *SimpleBounds) Limits() (float64, float64) {
+	return sb.Lower, sb.Upper
 
-    return sb.Lower, sb.Upper
-
-  }
-
+}
 
 /*
  *  Main INTree object;
@@ -59,96 +48,94 @@ type SimpleBounds struct {
  *
  *    @method buildTree:  internal tree construction function; called by NewINTree(); calls utility functions sort() and augment() to build node dependencies
  *    @method Including:  main public entry point: finds all bounds that include the given value
- */ 
+ */
 
 type INTree struct {
+	idxs []int
+	lmts []float64
+}
 
-  idxs     []int
-  lmts     []float64
+/*
+ *  Internal tree construction function
+ *
+ *    @param bnds: Slice of objects implementing Bounds[] interface
+ */
+
+func (inT *INTree) buildTree(bnds []Bounds) {
+
+	inT.idxs = make([]int, len(bnds))
+	inT.lmts = make([]float64, 3*len(bnds))
+
+	for i, v := range bnds {
+
+		inT.idxs[i] = i
+		l, u := v.Limits()
+
+		inT.lmts[3*i] = l
+		inT.lmts[3*i+1] = u
+		inT.lmts[3*i+2] = 0
+		
+	}
+
+	sort(inT.lmts, inT.idxs)
+	augment(inT.lmts, inT.idxs)
 
 }
 
+/*
+ *  Main public entry point for bounds searches
+ *
+ *    @param val: value to search containing bounds for
+ *
+ *    @return:    Slice holding all found array indices referencing the input Bounds[]
+ */
 
-  /*
-   *  Internal tree construction function
-   *    
-   *    @param bnds: Slice of objects implementing Bounds[] interface
-   */
+func (inT *INTree) Including(val float64) []int {
 
-  func (inT *INTree) buildTree(bnds []Bounds) {
-    
-    inT.idxs = make([]int, len(bnds))
-    inT.lmts = make([]float64, 3*len(bnds))
+	stk := []int{0, len(inT.idxs) - 1}
+	res := []int{}
 
-    for i, v := range bnds {
+	for len(stk) > 0 {
 
-      inT.idxs[i] = i
-      l, u := v.Limits()
+		rb := stk[len(stk)-1]
+		stk = stk[:len(stk)-1]
+		lb := stk[len(stk)-1]
+		stk = stk[:len(stk)-1]
 
-      inT.lmts[3*i]   = l
-      inT.lmts[3*i+1] = u
-      inT.lmts[3*i+2] = 0
+		if lb == rb+1 {
+			continue
+		}
 
-    }
+		cn := int(math.Ceil(float64(lb+rb) / 2.0))
+		nm := inT.lmts[3*cn+2]
 
-    sort(inT.lmts, inT.idxs)
-    augment(inT.lmts, inT.idxs)
+		if val <= nm {
 
-  }
+			stk = append(stk, lb)
+			stk = append(stk, cn-1)
 
+		}
 
-  /*
-   *  Main public entry point for bounds searches
-   *
-   *    @param val: value to search containing bounds for
-   *
-   *    @return:    Slice holding all found array indices referencing the input Bounds[]
-   */
+		l := inT.lmts[3*cn]
 
-  func (inT *INTree) Including(val float64) []int {
+		if l <= val {
 
-    stk := []int{0, len(inT.idxs) - 1}
-    res := []int{}
+			stk = append(stk, cn+1)
+			stk = append(stk, rb)
 
-    for len(stk) > 0 {
+			u := inT.lmts[3*cn+1]
 
-      rb  := stk[len(stk)-1]
-      stk  = stk[:len(stk)-1]
-      lb  := stk[len(stk)-1]
-      stk  = stk[:len(stk)-1]
+			if val <= u {
+				res = append(res, inT.idxs[cn])
+			}
 
-      if lb == rb + 1 { continue }
+		}
 
-      cn := int(math.Ceil(float64(lb + rb) / 2.0))
-      nm := inT.lmts[3*cn+2]
+	}
 
-      if val <= nm {
+	return res
 
-        stk = append(stk, lb)
-        stk = append(stk, cn - 1)
-
-      }
-
-      l := inT.lmts[3*cn]
-
-      if l <= val {
-
-        stk = append(stk, cn + 1)
-        stk = append(stk, rb)
-
-        u := inT.lmts[3*cn+1]
-        
-        if val <= u { res = append(res, inT.idxs[cn]) }
-
-      }
-
-    }
-
-    return res
-  
-  }
-
-
+}
 
 /*
  *  Main initialization function; creates the tree from passed in Bounds objects by calling method buildTree
@@ -160,14 +147,12 @@ type INTree struct {
 
 func NewINTree(bnds []Bounds) *INTree {
 
-  inT := INTree{}
-  inT.buildTree(bnds)
+	inT := INTree{}
+	inT.buildTree(bnds)
 
-  return &inT
+	return &inT
 
 }
-
-
 
 /*
  *  Utility function to augment tree by adding maximum value of all child nodes
@@ -178,25 +163,28 @@ func NewINTree(bnds []Bounds) *INTree {
 
 func augment(lmts []float64, idxs []int) {
 
-  if len(idxs) < 1 { return }
+	if len(idxs) < 1 {
+		return
+	}
 
-  max := 0.0
+	max := 0.0
 
-  for idx, _ := range idxs {
+	for idx := range idxs {
 
-    if lmts[3*idx+1] > max { max = lmts[3*idx+1] }
+		if lmts[3*idx+1] > max {
+			max = lmts[3*idx+1]
+		}
 
-  }
+	}
 
-  r := int(math.Floor(float64(len(idxs)) / 2.0))
+	r := int(math.Floor(float64(len(idxs)) / 2.0))
 
-  lmts[3*r+2] = max
+	lmts[3*r+2] = max
 
-  augment(lmts[:3*r], idxs[:r])
-  augment(lmts[3*r+3:], idxs[r+1:])
+	augment(lmts[:3*r], idxs[:r])
+	augment(lmts[3*r+3:], idxs[r+1:])
 
 }
-
 
 /*
  *  Utility function to sort tree by lowest limits
@@ -207,32 +195,34 @@ func augment(lmts []float64, idxs []int) {
 
 func sort(lmts []float64, idxs []int) {
 
-  if len(idxs) < 2 { return }
-    
-  l, r := 0, len(idxs) - 1
+	if len(idxs) < 2 {
+		return
+	}
 
-  p := rand.Int() % len(idxs)
+	l, r := 0, len(idxs)-1
 
-  idxs[p], idxs[r] = idxs[r], idxs[p]
-  lmts[3*p], lmts[3*p+1], lmts[3*p+2], lmts[3*r], lmts[3*r+1], lmts[3*r+2] = lmts[3*r], lmts[3*r+1], lmts[3*r+2], lmts[3*p], lmts[3*p+1], lmts[3*p+2]
+	p := rand.Int() % len(idxs)
 
-  for i := range idxs  {
+	idxs[p], idxs[r] = idxs[r], idxs[p]
+	lmts[3*p], lmts[3*p+1], lmts[3*p+2], lmts[3*r], lmts[3*r+1], lmts[3*r+2] = lmts[3*r], lmts[3*r+1], lmts[3*r+2], lmts[3*p], lmts[3*p+1], lmts[3*p+2]
 
-      if lmts[3*i] < lmts[3*r] {
+	for i := range idxs {
 
-        idxs[l], idxs[i] = idxs[i], idxs[l]
-        lmts[3*l], lmts[3*l+1], lmts[3*l+2], lmts[3*i], lmts[3*i+1], lmts[3*i+2] = lmts[3*i], lmts[3*i+1], lmts[3*i+2], lmts[3*l], lmts[3*l+1], lmts[3*l+2]
-        
-        l++
-      
-      }
-  
-  }
+		if lmts[3*i] < lmts[3*r] {
 
-  idxs[l], idxs[r] = idxs[r], idxs[l]
-  lmts[3*l], lmts[3*l+1], lmts[3*l+2], lmts[3*r], lmts[3*r+1], lmts[3*r+2] = lmts[3*r], lmts[3*r+1], lmts[3*r+2], lmts[3*l], lmts[3*l+1], lmts[3*l+2]
-    
-  sort(lmts[:3*l], idxs[:l])
-  sort(lmts[3*l+3:], idxs[l+1:])
-    
+			idxs[l], idxs[i] = idxs[i], idxs[l]
+			lmts[3*l], lmts[3*l+1], lmts[3*l+2], lmts[3*i], lmts[3*i+1], lmts[3*i+2] = lmts[3*i], lmts[3*i+1], lmts[3*i+2], lmts[3*l], lmts[3*l+1], lmts[3*l+2]
+
+			l++
+
+		}
+
+	}
+
+	idxs[l], idxs[r] = idxs[r], idxs[l]
+	lmts[3*l], lmts[3*l+1], lmts[3*l+2], lmts[3*r], lmts[3*r+1], lmts[3*r+2] = lmts[3*r], lmts[3*r+1], lmts[3*r+2], lmts[3*l], lmts[3*l+1], lmts[3*l+2]
+
+	sort(lmts[:3*l], idxs[:l])
+	sort(lmts[3*l+3:], idxs[l+1:])
+
 }
